2e8177bb76bb6f6afa89bd30f0ed07084834bb2c
Jabbrowocky <mindbull3tz@gmail.com>
2017-12-18 09:05:35 -0600

    added log file to the solution, routed streamwriter to file, stopped throwing i/o error

--- ChatroomStarter/ChatroomStarter/ChatroomStarter/Server/Log.cs
+++ ChatroomStarter/ChatroomStarter/ChatroomStarter/Server/Log.cs
@@ -10,7 +10,7 @@ namespace Server
     {
         public void WriteToLog(string text) //Dependency Inversion Principle, this log method writes to the log for every send/receive/log in/log out
         {
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Chez\Desktop\CSharp\TCPChat\ChatroomStarter\ChatroomStarter\ChatroomStarter\Log\log.txt", true))
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("log.txt", true))
             {
                 file.WriteLine(text);
             }
